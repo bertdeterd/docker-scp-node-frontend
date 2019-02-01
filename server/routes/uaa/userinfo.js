8c47aefd020fd155c2e992c6587c1de6507bc932
Bert Deterd <bertdeterd@outlook.com>
2019-02-01 09:13:29 +0100

    ecs

--- server/routes/uaa/userinfo.js
+++ server/routes/uaa/userinfo.js
@@ -1,13 +1,19 @@
 const request = require('request');
 
-module.exports =  (req, res) => {
+module.exports = (req, res) => {
 
-    request.get('http://userservice/uaa/userinfo', 
-    function (error, response, body) {
-        console.log(response);
-        console.log(body);
-        let resp = JSON.parse(body);
-        res.send(resp);
-    })
+    request.get('http://userservice/uaa/userinfo',
+        function (error, response, body) {
+
+            if (error) {
+                res.send(JSON.parse(error));
+            } else {
+                console.log(response);
+                console.log(body);
+                let resp = JSON.parse(body);
+                res.send(resp);
+            }
+
+        })
 
 };
